6924923a77b98b55fe06c8c1526885876a387f65
astemeric <48163922+astemeric@users.noreply.github.com>
2020-06-16 12:51:31 -0700

    Update Test_Script.py

--- Dexter/Test_Script.py
+++ Dexter/Test_Script.py
@@ -5,11 +5,11 @@ import time
 # +-334000 seems to be about 90 degrees for joints
 # +-3711 (334000/90) per degree
 
-#'M x y z xtheta ytheta ztheta right_arm elbow_up wrist_out
 #b before string denotes bytes
-#first four parameters are not used for arm command - probably just used by DDE
+#first four parameters are not used for arm command - probably just used by DDE - which is why they are 'xxx xxx xxx xxx'
+#'M x y z xtheta ytheta ztheta right_arm elbow_up wrist_out
 #'a theta1 theta2 theta3 theta4 theta5'
-
+#coordinates are in micrometers - so x = 100000 will be x = .1 meters
 
 ########## Old Notes ###########
 # dexter.connect((dex_ip, dex_port))  # not really sure when I need to do this, can i keep the connection open the
@@ -32,39 +32,17 @@ def connect():
     dexter = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     dexter.connect((dex_ip, dex_port))
 
-# dexter.close()  # this ends the connection
-
-#coordinates are in micrometers - so x = 100000 will be x = .1 meters
-
-#nx = 5
-#ny = 5
-#step = 5 * 1000
-#points = []
-#zero = [0, 500000, -100000]
-#topLeft = [zero[0]-0.5*step*(nx-1), zero[1]+0.5*step*(ny-1), zero[2]]   # Generate top left point of grid
-# Generate the grid points for Dexter to move to -> This makes an S pattern,
-# I can make it have different cases if we want to always go left to right or whatever
-
-#for i in range(int(float(ny))):                # Number of rows
-#    for j in range(int(float(nx))):            # Number of columns
-#        if i % 2 == 0:             # Check to see if row number is even
-#            points.append([round(topLeft[0]+j*step, -2), round(topLeft[1]-i*step, -2), round(topLeft[2], -2)])
-#        else:                      # If the row is odd, move right to left
-#            points.append([round(topLeft[0] + (nx-1)*step - j*step, -2), round(topLeft[1]-i*step, -2),
-#                           round(topLeft[2], -2)])
-
-
+#ignore
 def moveTo(coords):
-    # This way of encoding works!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     #dexter.send(b'xxx xxx xxx xxx M %(x)d %(y)d %(z)d 0 0 -1 1 1 1;'%{b'x':coords[0], b'y':coords[1], b'z':coords[2]})
+    #print(b'xxx xxx xxx xxx M %(x)d %(y)d %(z)d 0 0 -1 1 1 1;'%{b'x':coords[0], b'y':coords[1], b'z':coords[2]})
 
-    print(b'xxx xxx xxx xxx M %(x)d %(y)d %(z)d 0 0 -1 1 1 1;'%{b'x':coords[0], b'y':coords[1], b'z':coords[2]})
-
+#ignore
 def moveToHigh(coords):
     #dexter.send(b'xxx xxx xxx xxx M %(x)d %(y)d %(z)d 0 0 -1 1 1 1;' % {b'x': coords[0], b'y': coords[1], b'z': coords[2]+10000})
-    print(b'xxx xxx xxx xxx M %(x)d %(y)d %(z)d 0 0 -1 1 1 1;' % {b'x': coords[0], b'y': coords[1], b'z': coords[2]+10000})
+    #print(b'xxx xxx xxx xxx M %(x)d %(y)d %(z)d 0 0 -1 1 1 1;' % {b'x': coords[0], b'y': coords[1], b'z': coords[2]+10000})
 
-def moveToHigh():
+def move():
     #dexter.send(b'xxx xxx xxx xxx M 0 500000 100000 0 0 -1 1 1 1;')
     dexter.send(b'xxx xxx xxx xxx a 0 167000 -167000 0 0;')
     time.sleep(1)
